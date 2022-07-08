09a33b58ae6936f66098af63b427fc7866aef284
shalini <106244790+kichkandi@users.noreply.github.com>
2022-07-08 09:49:14 +0530

    Add files via upload

---
+++ q-2.py
@@ -0,0 +1,8 @@
+#*FIND ALPHABET.......
+c=input("enter character")
+if (c>="A" and c<"Z")or(c>='a' and c<='z'):
+  print("elphabet")
+elif (c>='0' and c<='9'):
+  print("digit")
+else:
+  print("special")
