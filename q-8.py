7523c010b53e6983d9e1d9841e2f3241dc52210a
shalini <106244790+kichkandi@users.noreply.github.com>
2022-07-08 10:33:41 +0530

    Update q-8.py

--- q-8.py
+++ q-8.py
@@ -1,12 +1,10 @@
 #.....Write a python program to check whether a character is an alphabet or not.
 
 
-s = input("enter the character:")
-if s=="a" or s=="e" or s=="i" or s=="o" or s=="u" or s=="A" or s=="E" or s=="I" or s=="O" or s=="U":
-  print("it is a vowel")
+s = input("enter a character:")
+if s>="a" and s<="z" or s>="A" and s<="Z":
+  print("alphabet")
 elif s>="0" and s<="9":
-  print("please enter the alphabet")
+  print("number")
 else:
-  print("consonent")
-
-  
+  print("special character")
