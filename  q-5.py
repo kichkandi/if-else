5a93235b1683a2244cfc0284252b86824ca1718f
shalini <106244790+kichkandi@users.noreply.github.com>
2022-07-08 10:09:08 +0530

    Add files via upload

---
+++  q-5.py
@@ -0,0 +1,8 @@
+#...Write a python program to check whether a number is divisible by 5 and 11 or not.
+
+a = int(input("enter any number:"))
+if a%5==0 and a%11==0:
+  print("this is divisibl by 5 and 11.")
+else:
+  print("not divided.")
+ 
