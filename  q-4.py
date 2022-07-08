cd9da106a8c0d89769a49895b9beacd513fe2d8a
shalini <106244790+kichkandi@users.noreply.github.com>
2022-07-08 10:06:27 +0530

    Add files via upload

---
+++  q-4.py
@@ -0,0 +1,9 @@
+#..Write a python program to check whether a number is negative, positive or zero.
+
+s = int(input("enter any number:"))
+if s == 0:
+  print("this number is equel to zero.")
+elif s < 0:
+  print("this is negative number.")
+else:
+  print("this is positive number.")
